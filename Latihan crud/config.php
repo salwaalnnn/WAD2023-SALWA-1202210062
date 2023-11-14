701109d1ca4ce575b68e4175bf8abaa3d0163584
salwaalnnn <salwaalnnn@student.telkomuniversity.ac.id>
2023-11-15 05:38:51 +0700

    gis

--- Latihan crud/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost:3306", "root", "", "testingdatabase");
-?>
--- Latihan crud/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("config.php");
-    $id = $_GET['id'];
-    mysqli_query($connect, "DELETE from pengguna WHERE id=$id");
-    header("Location: read.php");
-?>
